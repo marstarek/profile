015c30444c62d302e77d7e43791193d352b6f275
Tarek <51708388+marstarek@users.noreply.github.com>
2021-12-21 03:27:22 +0200

    pagination ul & li

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
-let sliderImags = Array.from(
+// Get Slider Items | Array.form [ES6 Feature]
+var sliderImages = Array.from(
     document.querySelectorAll(".slider-container img")
 );
-let slidsCount = sliderImags.length;
+
+// Get Number Of Slides
+var slidesCount = sliderImages.length;
+
+// Set Current Slide
+var currentSlide = 1;
+
+// Slide Number Element
+var slideNumberElement = document.getElementById("slide-number");
+
+// Previous and Next Buttons
+var nextButton = document.getElementById("next");
+var prevButton = document.getElementById("prev");
+
+// Handle Click on Previous and Next Buttons
+nextButton.onclick = nextSlide;
+prevButton.onclick = prevSlide;
+//creat ul
+let paginationElement = document.createElement("ul");
+paginationElement.setAttribute("id", "pagination-ul");
+for (let index = 1; index <= slidesCount; index++) {
+    let paginationItem = document.createElement("li");
+    paginationItem.setAttribute("data-index", index);
+    paginationItem.appendChild(document.createTextNode(index));
+    paginationElement.appendChild(paginationItem);
+}
+//add created ul to page
+document.getElementById("indicators").appendChild(paginationElement);
+
+function nextSlide() {
+    console.log("prev");
+}
+
+function prevSlide() {
+    console.log("d");
+}
